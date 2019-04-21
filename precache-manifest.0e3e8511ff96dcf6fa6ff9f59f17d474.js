bdce35c977efee7d12c892012e6ccd19fd38771d
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-04-21 17:27:52 +0200

    Updates

---
+++ precache-manifest.0e3e8511ff96dcf6fa6ff9f59f17d474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13165afa092f695a476b",
+    "url": "/tildea-public-build/static/js/runtime~main.d5b99ee2.js"
+  },
+  {
+    "revision": "91016aa2cb9d0ed0ab48",
+    "url": "/tildea-public-build/static/js/main.a713538b.chunk.js"
+  },
+  {
+    "revision": "e4794fbb3018324a1da2",
+    "url": "/tildea-public-build/static/js/2.a4cae522.chunk.js"
+  },
+  {
+    "revision": "91016aa2cb9d0ed0ab48",
+    "url": "/tildea-public-build/static/css/main.bdbd3b5a.chunk.css"
+  },
+  {
+    "revision": "d5572e58340ec4c147fd8f03a8040455",
+    "url": "/tildea-public-build/index.html"
+  }
+];
